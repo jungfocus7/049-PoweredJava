89e7afeff8f5725994641ccebc97813e8eb77840
JUNGFOCUS7 <jungfocus7@gmail.com>
2025-06-25 07:32:50 +0900

    원시배열관련 공부 그리고 업데이트

---
+++ 250619/javahbx/src/hbx/helpers/PrimitiveHelper.java
@@ -0,0 +1,123 @@
+package hbx.helpers;
+
+public final class PrimitiveHelper {
+	public static Byte[] toByteArray(byte[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Byte[] rra = new Byte[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Byte.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Short[] toShortArray(short[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Short[] rra = new Short[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Short.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Integer[] toIntegerArray(int[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Integer[] rra = new Integer[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Integer.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Long[] toLongArray(long[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Long[] rra = new Long[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Long.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Float[] toFloatArray(float[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Float[] rra = new Float[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Float.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Double[] toDoubleArray(double[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Double[] rra = new Double[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Double.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Character[] toCharacterArray(char[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Character[] rra = new Character[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Character.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+
+	public static Boolean[] toBooleanArray(boolean[] arr) {
+		if ((arr == null) || (arr.length == 0)) {
+			return null;
+		}
+
+		int l = arr.length;
+		Boolean[] rra = new Boolean[l];
+
+		for (int i = 0; i < l; i++) {
+			rra[i] = Boolean.valueOf(arr[i]);
+		}
+
+		return rra;
+	}
+}
--- 250619/javahbx_testers/src/TesterProgram.java
+++ 250619/javahbx_testers/src/TesterProgram.java
@@ -1,5 +1,11 @@
 import java.lang.reflect.Array;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 import hbx.dataLoaders.ConfigDataLoader;
 import hbx.helpers.StringHelper;
@@ -91,12 +97,18 @@ public final class TesterProgram {
 	}
 
 	public static void main(String[] args) {
+		/*
 		testStringHelper();
 
 		testConfigDataLoader();
 
 
 		testTemper();
+
+		testTemper31();
+		*/
+
+		testTemper32();
 	}
 
 
@@ -106,6 +118,133 @@ public final class TesterProgram {
 	private static void testTemper() {
 		int l = Array.getLength(new String[] { "박종명" });
 		println(">>> " + l);
+	}
+
+	private static void testTemper31() {
+//		String name = "박종명";
+//		println("::" + "임헌진".compareTo(name));
+//		println("::" + ("b".compareTo("c")));
+//		if ("임헌진".compareTo(name) < 0)
+//		{
+//
+//		}
+
+//		String[] nums = { "2", "3", "" };
+		String nums = "8012983014";
+		char[] cha = nums.toCharArray();
+
+		Arrays.sort(cha);
+
+
+		List<String> lst = new ArrayList<String>();
+		lst.add("03) 박종명");
+		lst.add("02) 임헌진");
+		lst.add("01) 이중호");
+		lst.sort(new Comparator<String>() {
+			@Override
+			public int compare(String tx, String ty) {
+				int rx = tx.compareTo(ty);
+				if (rx < 0) {
+					return 1;
+				}
+				else if (rx < 0) {
+					return -1;
+				}
+				else {
+					return 0;
+				}
+			}
+		});
+
+
+		println(">>>");
+
+	}
+
+
+	private static void testTemper32() {
+//		String[] nums = new String[] { "9", "8", "7", "6", "5", "4", "4", "3", "3", "1", "1", "9" };
+//		ArrayList<String> nums = { "0", "1", "2", "3", "3", "2" };
+
+//		char[] nums = "98765432109955331223777".toCharArray();
+		String numStr = "98765432109955331223777";
+//		Character[] nums = "98765432109955331223777".chars().mapToObj(tx -> Character.valueOf(tx)).toArray(Character[]::new);
+		char[] cha = numStr.toCharArray();
+		Character[] nums = new Character[cha.length];
+		for (int i = 0; i < cha.length; i++) {
+			nums[i] = Character.valueOf(cha[i]);
+		}
+
+		List<Character> lst = new ArrayList<Character>();
+		for (char num : nums) {
+			lst.add(num);
+		}
+
+		LinkedHashSet<Character> lhs = new LinkedHashSet<Character>();
+		lhs.addAll(lst);
+
+		HashSet<Character> hss = new HashSet<Character>();
+		hss.addAll(lst);
+
+
+//		Arrays.stream(nums).;
+
+		println(">>>");
+
+
+
+
+//        char[] originalArray = {1, 2, 2, 3, 4, 4, 5};
+//        char[] distinctArray = Arrays.stream(originalArray)
+//                                   .distinct()
+//                                   .toArray();
+
+		Object[] objs = nums;
+		Arrays.stream(objs);
+
+	}
+
+
+	private static void testTemper33() {
+		String numstr = "98765432109955331223777";
+		char[] nums = numstr.toCharArray();
+
+		List<char> xx = Arrays.asList(nums);
+
+		Character[] cha = new Character[cha.length];
+		for (int i = 0; i < cha.length; i++) {
+			nums[i] = Character.valueOf(cha[i]);
+		}
+
+
+
+		List<Character> lst = new ArrayList<Character>();
+		for (char num : nums) {
+			lst.add(num);
+		}
+
+		LinkedHashSet<Character> lhs = new LinkedHashSet<Character>();
+		lhs.addAll(lst);
+
+		HashSet<Character> hss = new HashSet<Character>();
+		hss.addAll(lst);
+
+
+//		Arrays.stream(nums).;
+
+		println(">>>");
+
+
+
+
+//        char[] originalArray = {1, 2, 2, 3, 4, 4, 5};
+//        char[] distinctArray = Arrays.stream(originalArray)
+//                                   .distinct()
+//                                   .toArray();
+
+		Object[] objs = nums;
+		Arrays.stream(objs);
+
 	}
 }
 
