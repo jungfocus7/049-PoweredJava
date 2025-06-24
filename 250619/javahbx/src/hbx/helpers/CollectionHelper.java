8341b4a90d40fd73fda12a1a9efc1c849992d096
JUNGFOCUS7 <jungfocus7@gmail.com>
2025-06-25 07:46:47 +0900

    ``

--- 250619/javahbx/src/hbx/helpers/CollectionHelper.java
+++ 250619/javahbx/src/hbx/helpers/CollectionHelper.java
@@ -1,17 +1,18 @@
 package hbx.helpers;
 
+import java.util.Collection;
 import java.util.List;
 
 
 public final class CollectionHelper {
 	private CollectionHelper() { }
 
-	public static <T> boolean isEmpty(List<T> lst) {
-		return (lst == null) || (lst.size() == 0);
+	public static <T> boolean isEmpty(Collection<T> clt) {
+		return (clt == null) || (clt.size() == 0);
 	}
 
-	public static <T> boolean isNotEmpty(List<T> lst) {
-		return (lst != null) && (lst.size() > 0);
+	public static <T> boolean isNotEmpty(Collection<T> clt) {
+		return (clt != null) && (clt.size() > 0);
 	}
 
 	public static <T> T get(List<T> lst, int i, T d) {
